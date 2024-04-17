bf3972448e61d4add6e2215c187865b30f3e0777
app.chuva <alanagabriela9@gmail.com>
2024-04-17 11:48:30 -0300

    inicio

---
+++ app.chuva/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace app.chuva;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ app.chuva/Results.cs
@@ -0,0 +1,8 @@
+namespace app.chuva;
+
+public class Results
+{
+    public int temp {get;set;}
+    public string description {get;set;}
+    public string city {get;set;}
+}
